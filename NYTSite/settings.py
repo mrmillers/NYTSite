ebdaa644a74373528ff0d434cc29d47bde0ba26d
Yan Zhang <zhangyanjun418@gmail.com>
2015-04-21 22:19:23 -0400

    Migrate SQL

--- NYTSite/settings.py
+++ NYTSite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'NYTSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nyt',
+		'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -87,7 +91,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
